5ff370ef17f81d2833d3d1ccfd7a4f449e3e6105
dgoldfeder13 <114775261+dgoldfeder13@users.noreply.github.com>
2025-02-07 21:02:21 +0000

    Website done

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,47 @@
+class ZebraController < ApplicationController
+  def rock
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    else 
+      @outcome = "won"
+    end
+    render({ :template => "game_templates/play_rock" })
+  end
+
+  def paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+    
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    else 
+      @outcome = "lost"
+    end
+    render({ :template => "game_templates/play_paper"})
+  end
+
+  def scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+   
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    else 
+      @outcome = "tied"
+    end
+    render({ :template => "game_templates/play_scissors"})
+
+  end
+
+  def home
+    render({ :template => "game_templates/homepage"})
+  end
+end 
+
+    
+    
